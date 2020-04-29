59173b92bbba87d16a0aa8a34fec983c7f36e6ae
Jahid Hasan Rony <61205383+jahidrony037@users.noreply.github.com>
2020-04-29 13:40:54 -0700

    outer scope acess

---
+++ scope.js
@@ -0,0 +1,25 @@
+let bonus =20;
+
+function sum (num1,num2)
+{
+    
+    let result= num1 + num2 + bonus;
+   // console.log(bonus);
+   
+
+    if(result >9){
+       var mood ="happy";
+        mood ="panky";
+        mood ="canky";
+        mood = "franky";
+        console.log(mood);
+    }
+    console.log(mood);
+    
+    const day = "friday"
+    console.log(day);
+    return result;
+}
+const outPut =sum(12,12);
+// console.log(bonus);
+// console.log(outPut);
