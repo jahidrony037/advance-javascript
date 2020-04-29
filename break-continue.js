0675543e7d2ec70cfcbc992ad07dfe2a50ba0aa2
Jahid Hasan Rony <61205383+jahidrony037@users.noreply.github.com>
2020-04-29 14:46:14 -0700

    break and continue

---
+++ break-continue.js
@@ -0,0 +1,20 @@
+const numbers = [1,-2,2,-3,3,4,5,6,7,8,9];
+
+for (let i = 0; i < numbers.length; i++) {
+    
+
+    if ( numbers[i] > 5) {
+        break;
+        
+    }
+   // console.log(numbers[i]);
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i]<0) {
+        continue;
+    }
+    console.log(numbers[i]);
+
+}
+
