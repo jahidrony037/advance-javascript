d5b7d893cab646e1979c238c026e97a77a81a08f
Jahid Hasan Rony <61205383+jahidrony037@users.noreply.github.com>
2020-04-29 10:03:46 -0700

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//false value
+//false
+// const name ="0";
+//0;
+//undifined;
+//""
+//"null"
+
+//truthty value 
+//{},[],num=5,"0"," ";
+
+let name=12;
+console.log(name);
+if(name || name==0){
+    console.log("condition is true ");
+}
+else 
+{
+    console.log("condition is false ");
+}
