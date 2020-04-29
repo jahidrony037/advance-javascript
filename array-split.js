e21d966eb5194f7ab523452c4af47422bcb00b7c
Jahid Hasan Rony <61205383+jahidrony037@users.noreply.github.com>
2020-04-29 14:22:24 -0700

    slice,splice,join concepts

---
+++ array-split.js
@@ -0,0 +1,11 @@
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+// const part =numbers.slice(2, 6);
+
+const removed = numbers.splice(2, 5, 77);
+ 
+// console.log(removed);
+// console.log(numbers);
+
+const together = numbers.join(" ami");
+console.log(together);
