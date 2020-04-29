0d6db790750c2953e5b4ba1e4d67e7f6e75681d2
Jahid Hasan Rony <61205383+jahidrony037@users.noreply.github.com>
2020-04-29 10:29:27 -0700

    null vs undifined cleared for interview

---
+++ nul-vs-undifined.js
@@ -0,0 +1,21 @@
+let pakhi;
+function add(num1,num2)
+{
+   console.log(num1+num2);
+   return 
+}
+
+function add(num1,num2) {
+    console.log(num1,num2)
+    
+}
+
+
+const premik ={name:"smart dude", phone:0215776};
+ console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages=[12,34,56];
+console.log(ages[4]);
